059f450d173000c13a88866684eb4aace15fdd7a
Guy Fattal <guyfattal10@gmail.com>
2021-09-05 08:55:01 +0300

    add groups by word route

--- src/controllers/wordsController.ts
+++ src/controllers/wordsController.ts
@@ -25,5 +25,20 @@ const getWordByID = (req: Request, res: Response, next: NextFunction) => {
     }
   );
 };
+const getWordGroups = (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  db.query(
+    `SELECT distinct(groups_words.group_name)
+    FROM groups_words
+    where groups_words.WordID="${id}";`,
+    (err, result) => {
+      if (err) {
+        res.status(401).json({ message: "fatal error", error: err });
+      } else {
+        res.status(200).json({ result });
+      }
+    }
+  );
+};
 
-export { getAllWords, getWordByID };
+export { getAllWords, getWordByID, getWordGroups };
--- src/routes/wordsRoutes.ts
+++ src/routes/wordsRoutes.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { getAllWords, getWordByID } from "../controllers/wordsController";
+import {
+  getAllWords,
+  getWordByID,
+  getWordGroups,
+} from "../controllers/wordsController";
 
 const router = express.Router();
 
 router.get("/", getAllWords);
 router.get("/:id", getWordByID);
+router.get("/groups/:id", getWordGroups);
 
 export default router;
